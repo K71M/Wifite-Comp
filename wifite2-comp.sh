4d1c3b069bfc48af709e3b472a8b09087e579b2c
K71M <50496698+K71M@users.noreply.github.com>
2022-04-04 09:45:22 -0500

    Add files via upload

---
+++ wifite2-comp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+negro="\033[1;30m"
+azul="\033[1;31m"
+verde="\033[1;32m"
+amarillo="\033[1;33m"
+azul="\033[1;34m"
+morado="\033[1;35m"
+cian="\033[1;36m"
+blanco="\033[1;37m"
+
+
+echo -e $azul " ██████╗  █████╗  ██████╗██╗  ██╗ █████╗  ██████╗ ███████╗███████╗ "
+echo -e $azul " ██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔══██╗██╔════╝ ██╔════╝██╔════╝ "
+echo -e $azul " ██████╔╝███████║██║     █████╔╝ ███████║██║  ███╗█████╗  ███████╗ "
+echo -e $azul " ██╔═══╝ ██╔══██║██║     ██╔═██╗ ██╔══██║██║   ██║██╔══╝  ╚════██║ "
+echo -e $azul " ██║     ██║  ██║╚██████╗██║  ██╗██║  ██║╚██████╔╝███████╗███████║ "
+echo -e $azul " ╚═╝     ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝ "
+echo -e $azul " 			WIFITE2-KTYM-BETA			  "
+
+apt install aircrack-ng -y
+apt install tshark -y
+apt install macchanger -y
+apt install reaver -y
+apt install bully -y
+apt install hashcat -y
